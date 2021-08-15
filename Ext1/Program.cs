ef3109e82faa3912eed1955859631ed9db39bdb4
Yuvenaliyt <yuvenaliy@gmail.com>
2021-08-15 10:47:17 +0400

    Добавьте файлы проекта.

---
+++ Ext1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите свой рост");
+            int growth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите свой вес");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Вас зовут: {name} {surname}, ваш возраст: {age}, ваш рост: {growth}, ваш вес: {height}");
+        }
+    }
+}
