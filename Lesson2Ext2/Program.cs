f1efaaad0d841e5e9f4f8f7119e216bb7e5f6f83
Yuvenaliyt <yuvenaliy@gmail.com>
2021-08-15 14:58:55 +0400

    Lesson2Ext2

---
+++ Lesson2Ext2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson2Ext2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Написать метод подсчета количества цифр числа.
+
+            Console.Write("Введите число : ");
+            int num = int.Parse(Console.ReadLine());
+            CountNumber(ref num);
+
+        }
+
+        private static int CountNumber(ref int num)
+        {
+            int i = 0;
+            while (num > 0)
+            {
+                i++;
+                num /= 10;
+            }
+            Console.WriteLine("Количество цифр введенного числа : {0}", i);
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
